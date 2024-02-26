ec92a46f3c445cde44822f57885ce7838ffc9b88
Carlos S. Córdoba Delgado <cs.cordoba@gmail.com>
2024-02-25 21:24:31 -0600

    Semana 6
    Tienda actualizada a semana 6

--- src/main/java/com/tienda/dao/CategoriaDao.java
+++ src/main/java/com/tienda/dao/CategoriaDao.java
@@ -2,6 +2,7 @@ package com.tienda.dao;
 
 import com.tienda.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
- 
+
 public interface CategoriaDao extends JpaRepository <Categoria,Long> {
+
 }
